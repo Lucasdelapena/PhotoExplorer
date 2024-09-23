6b403e33c0a6484b91c8de2a8bd5d7bd63c25800
Lucasdelapena <lucas.delapena@outlook.com>
2024-09-23 17:29:14 -0500

    bringing things in from the old project

--- Assignment2_CS4420.py
+++ Assignment2_CS4420.py
@@ -7,6 +7,7 @@ import cv2
 import numpy as np
 import random
 import argparse
+from screeninfo import get_monitors
 
 #selfnote: when getting pictures make sure to get copyright free and location of where you receieved the picture
 
@@ -17,6 +18,7 @@ def main():
     parser.add_argument('-g') # save the output image as grayscale (default: save as input)
     parser.add_argument('-rows', type=int, default=0) # -r max number of rows
     parser.add_argument('-cols', type=int, default=0) # -c max number of cols
+    parser.add_argument('-t') #output image type (jpg, tif, bmp, or png)
     parser.add_argument('indir') # input directory
     parser.add_argument('outdir') # output directory [default: indir.corpus]
     args = parser.parse_args()
@@ -24,6 +26,28 @@ def main():
     cols = args.cols
     OrginRows = rows # made for dimensions
     OrginCols = cols
+
+    # Get monitor information
+    for m in get_monitors(): #note: this gets takes the last monitor info
+        maxWidth = m.width
+        maxHeight = m.height
+    
+     # Check if rows and cols are greater than the monitor size
+    if rows > maxWidth:
+        rows = maxWidth - 200
+    if cols > maxHeight:
+        cols = maxHeight - 200
+
+    # lists and directory
+    dir = args.dir
+    picslist = []
+    filenameList = []
+
+    # gets directory path, directory names, and file names
+    for dirpath, dirnames, filenames in os.walk(dir, topdown=False): 
+        for filename in filenames:
+            picslist.append(os.path.join(dirpath, filename))
+            filenameList.append(filename)
 
     
 
